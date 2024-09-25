4803e77cd4e8b4984b3f06caa4e351188d6221e1
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-09-25 14:05:25 -0600

    Adding login and dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige la ruta vacía a 'home'
+  { path: 'login', component: LoginComponent }, // Ruta para el componente Login
+  { path: 'dashboard', component: DashboardComponent }, // Ruta para el componente Dasboard
+];
