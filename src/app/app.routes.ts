f320e11b17fbd5655d0c360cf11b8aa7b766ceb6
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-09-25 20:02:10 -0600

    Adding errors messages in login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige la ruta vacía a 'home'
-  { path: 'login', component: LoginComponent }, // Ruta para el componente Login
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirige la ruta vacía a 'home'
   { path: 'dashboard', component: DashboardComponent }, // Ruta para el componente Dasboard
+  { path: 'login', component: LoginComponent }, // Ruta para el componente Login
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.username && this.password && this.password.length > 5) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      console.error('Por favor, complete todos los campos correctamente.');
+    }
+  }
 }
